2dd4c64ef6032c78cb1ef82a53060c80c0d9660a
Finn Merlett <finn.merlett@gmail.com>
2021-12-14 14:57:36 +0000

    complete coding challenge

--- typescript/src/index.spec.ts
+++ typescript/src/index.spec.ts
@@ -1,4 +1,4 @@
-import { getResults } from ".";
+import { getResults, ParamTypeError } from ".";
 
 const givenDivision = [
   {
@@ -23,6 +23,29 @@ const givenDivision = [
   },
 ];
 
+const badDivision1 = [
+  {
+    name: "Rockets",
+    points: -1,
+  },
+];
+
+const badDivision2 = [
+  {
+    name: 4,
+    points: 7,
+  },
+];
+
+const badDivision3 = [
+  {
+    name: "Muskets",
+    points: null,
+  },
+];
+
+const mixedBadDivision = givenDivision.concat(badDivision1);
+
 test("returns one team to promote and one team to relegate", () => {
   const resultString = `Promote:
 Cardinals
@@ -43,4 +66,24 @@ Bruisers
 Renegades`;
 
   expect(getResults(givenDivision, 2)).toBe(resultString);
+});
+
+test("catches an 'n' value too large for the given division", () => {
+  expect(() => getResults(givenDivision, 4)).toThrow(/n is too large/);
+});
+
+test("catches incorrect 'division' or 'n' parameters", () => {
+  expect(() => getResults(badDivision1, 1)).toThrow(/parameter 'division'/);
+  expect(() => getResults(badDivision2, 1)).toThrow(/parameter 'division'/);
+  expect(() => getResults(badDivision3, 1)).toThrow(/parameter 'division'/);
+  expect(() => getResults(mixedBadDivision, 3)).toThrow(/parameter 'division'/);
+  expect(() => getResults(givenDivision, -2)).toThrow(/n must be 0 or greater/);
+  expect(() => getResults(givenDivision, "oops")).toThrow(/parameter 'n'/);
+  expect(() => getResults(givenDivision, 10)).toThrow(/n is too large/);
+  expect(() =>
+    getResults(Array(4).fill(givenDivision).flat(), 3)
+  ).toThrow(/must contain between 2 and 16/);
+  expect(() => getResults(givenDivision.slice(0, 1), 3)).toThrow(
+    /must contain between 2 and 16/
+  );
 });
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,72 @@
+export class ParamTypeError extends Error {
+  constructor(name: string, expectedType: string) {
+    super(`parameter '${name}' must be of type ${expectedType}`);
+    this.name = "ParamTypeError";
+  }
+}
+
+type Team = {
+  name: string;
+  points: number;
+};
+
+const get = <T>(input: T, key: string): unknown => {
+  if (typeof input !== "object" || input === null) {
+    throw new Error("invalid inputs to 'get' method");
+  }
+  return input?.[key as keyof T];
+};
+
+const isTeam = (team: unknown): team is Team => {
+  if (typeof team !== "object" || team === null) return false;
+
+  const name = get(team, "name");
+  const points = get(team, "points");
+  return (
+    typeof name === "string" &&
+    typeof points === "number" &&
+    Number.isInteger(points) &&
+    points >= 0
+  );
+};
+
+const isDivision = (division: unknown[]): division is Team[] =>
+  division.every(isTeam);
+
+// NOTE: For a rigorous solution (probably beyond the expected scope),
+// I wrote exhaustive parameter type checks. The quick version was done
+// well within the time limit, and this version took me a little longer.
+
+// In a workplace environment I would check with a supervisor to clarify
+// the scope of the task beforehand, and if the input types were validated
+// prior to this function I would type the parameters accordingly and write
+// only the validators needed (as in the short version).
+
 export const getResults = (division: unknown, n: unknown) => {
-  // Your code here
+  if (!Array.isArray(division)) throw new ParamTypeError("division", "array");
+  if (!isDivision(division)) throw new ParamTypeError("division", "team array");
+  if (!(typeof n === "number")) throw new ParamTypeError("n", "number");
+  if (!Number.isInteger(n)) throw new Error("n must be an integer");
+  if (n < 0) throw new Error("n must be 0 or greater");
+  if (division.length < 2 || division.length > 16) {
+    throw new Error("division must contain between 2 and 16 teams");
+  }
+  if (n >= division.length) {
+    throw new Error("n is too large for the given division");
+  }
+
+  // sort in descending points order (largest first)
+  division.sort((teamA, teamB) => teamB.points - teamA.points);
+  const toPromote = division.slice(0, n); // get top n teams
+  const toRelegate = division.slice(-n); // get bottom n teams
+
+  if (toPromote.some((teamToPromote) => toRelegate.includes(teamToPromote))) {
+    throw new Error("n is too large for the size of the given division");
+  }
+
+  return `Promote:
+${toPromote.map((t) => t.name).join("\n")}
+
+Relegate:
+${toRelegate.map((t) => t.name).join("\n")}`;
 };
